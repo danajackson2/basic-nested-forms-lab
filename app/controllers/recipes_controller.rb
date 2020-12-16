166cf4527f7c09a6acdf8653dcf9bb7f4c567f8c
danajackson2 <danajackson2@gmail.com>
2020-12-16 15:43:35 -0800

    new ingredients in recipe controller file

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,8 +9,26 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    @recipe = Recipe.create(recipe_params)
+      ingredients_attributes.each do |k,v|
+        Ingredient.create(name: v["name"], quantity: v["quantity"], recipe_id: @recipe.id)   
+      end
+    redirect_to @recipe
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title)
+  end
+
+  def ingredients_attributes
+    params["recipe"]["ingredients_attributes"]
+  end
+
 end
